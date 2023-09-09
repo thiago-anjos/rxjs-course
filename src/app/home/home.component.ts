938a49285fbdd64c4b55b74470e346e0151d5695
thiago <thiago.w3c@gmail.com>
2023-09-09 17:43:35 -0300

    add shareReplay operators wich prevent the re-execution of http requests unecessary, in this example the request now is just one and before add sharereplay, it has just one

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Course } from '../model/course'
-import { interval, Observable, of, timer } from 'rxjs'
-import {
-    catchError,
-    delayWhen,
-    map,
-    retryWhen,
-    shareReplay,
-    tap,
-} from 'rxjs/operators'
+import { Observable } from 'rxjs'
+import { map, shareReplay, tap } from 'rxjs/operators'
 import { createHttpObservable } from '../utils/httpObservable'
 import { noop } from 'rxjs'
 
@@ -26,10 +19,12 @@ export class HomeComponent implements OnInit {
     ngOnInit() {
         const http$ = createHttpObservable('/api/courses')
 
-        const courses$: Observable<Course[]> = http$.pipe(
+        const courses$ = http$.pipe(
+            tap(() => console.log('http request executed')),
             // map(res => res["payload"])
-            map((res) => Object.values(res['payload'])) // is the same above
-        )
+            map((res) => Object.values(res['payload'])), // is the same above
+            shareReplay()
+        ) as Observable<Course[]>
 
         this.beginnersCourses$ = courses$.pipe(
             map((courses) =>
