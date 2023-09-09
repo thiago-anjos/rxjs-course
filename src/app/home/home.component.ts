5ab350e70c70ec9edb7081ebc9daca4546daf9e9
thiago <thiago.w3c@gmail.com>
2023-09-09 17:35:26 -0300

    observable stream of data, using async pipe on angular, it also automatic unsubscribe on this way

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import { createHttpObservable } from '../utils/httpObservable';
-import { noop } from 'rxjs';
-
+import { Component, OnInit } from '@angular/core'
+import { Course } from '../model/course'
+import { interval, Observable, of, timer } from 'rxjs'
+import {
+    catchError,
+    delayWhen,
+    map,
+    retryWhen,
+    shareReplay,
+    tap,
+} from 'rxjs/operators'
+import { createHttpObservable } from '../utils/httpObservable'
+import { noop } from 'rxjs'
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-    beginnersCourses: Course[];
-    advancedCourses: Course[];
+    beginnersCourses$: Observable<Course[]>
+    advancedCourses$: Observable<Course[]>
 
     constructor() {}
 
     ngOnInit() {
-        const http$ = createHttpObservable('/api/courses');
+        const http$ = createHttpObservable('/api/courses')
 
-        const courses$ = http$.pipe(
+        const courses$: Observable<Course[]> = http$.pipe(
             // map(res => res["payload"])
-            map(res => Object.values(res["payload"])) // is the same above
+            map((res) => Object.values(res['payload'])) // is the same above
         )
-    
-        courses$.subscribe(
-          courses => {
-            this.beginnersCourses = courses.filter((course: Course) => course.category === "BEGINNER") as any;
-            this.advancedCourses = courses.filter((course: Course) => course.category === "ADVANCED") as any;
-          },
-          noop,
-          () => console.log('complete')
+
+        this.beginnersCourses$ = courses$.pipe(
+            map((courses) =>
+                courses.filter(
+                    (course: Course) => course.category === 'BEGINNER'
+                )
+            )
+        )
+        this.advancedCourses$ = courses$.pipe(
+            map((courses) =>
+                courses.filter(
+                    (course: Course) => course.category === 'ADVANCED'
+                )
+            )
         )
     }
-
 }
--- src/app/utils/httpObservable.ts
+++ src/app/utils/httpObservable.ts
@@ -1,17 +1,18 @@
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs'
+import { Course } from '../model/course'
 
-export function createHttpObservable(url:string){
-    return new Observable((subscriber)=>{
-      fetch(url)
-        .then(res =>{
-          return res.json()
-        })
-        .then(body =>{
-          subscriber.next(body)
-          subscriber.complete();
-        })
-        .catch(err =>{
-          subscriber.error(err)
-        })
+export function createHttpObservable(url: string) {
+    return new Observable<Course[]>((subscriber) => {
+        fetch(url)
+            .then((res) => {
+                return res.json()
+            })
+            .then((body) => {
+                subscriber.next(body)
+                subscriber.complete()
+            })
+            .catch((err) => {
+                subscriber.error(err)
+            })
     })
-};
+}
