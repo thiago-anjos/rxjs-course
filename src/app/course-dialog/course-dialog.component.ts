7171761262a907f529dcce6752e819d3363da872
thiago <thiago.w3c@gmail.com>
2023-10-22 18:40:22 -0300

    prevent multiple clicks from user

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -4,6 +4,7 @@ import {
     ElementRef,
     Inject,
     OnInit,
+    Renderer2,
     ViewChild,
     ViewEncapsulation,
 } from '@angular/core'
@@ -12,13 +13,7 @@ import { Course } from '../model/course'
 import { FormBuilder, Validators, FormGroup } from '@angular/forms'
 import * as moment from 'moment'
 import { fromEvent } from 'rxjs'
-import {
-    concatMap,
-    distinctUntilChanged,
-    exhaustMap,
-    filter,
-    mergeMap,
-} from 'rxjs/operators'
+import { concatMap, tap, exhaustMap, filter, mergeMap } from 'rxjs/operators'
 import { fromPromise } from 'rxjs/internal-compatibility'
 
 @Component({
@@ -30,14 +25,16 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     form: FormGroup
     course: Course
 
-    @ViewChild('saveButton', { static: true }) saveButton: ElementRef
+    @ViewChild('saveButton') saveButton: ElementRef
 
-    @ViewChild('searchInput', { static: true }) searchInput: ElementRef
+    @ViewChild('searchInput') searchInput: ElementRef
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course: Course
+        @Inject(MAT_DIALOG_DATA) course: Course,
+        private renderer: Renderer2,
+        private el: ElementRef
     ) {
         this.course = course
 
@@ -52,6 +49,7 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     ngOnInit() {
         this.form.valueChanges
             .pipe(
+                tap(() => console.log('formulario valido', this.form.valid)),
                 // Call pipe to manipulate the stream, then use filter to valid the form, very usefull this technique
                 filter(() => this.form.valid),
                 // call the saveCourse method with is observables. But concatMap calls then in sequencial, only when the last one has returned
@@ -74,10 +72,18 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
         )
     }
 
-    ngAfterViewInit() {}
+    ngAfterViewInit() {
+        const buttonElement = document.querySelector('#savedButton')
+        fromEvent(buttonElement, 'click')
+            // it prevents multiples requests to the server
+            .pipe(exhaustMap(() => this.saveCourse(this.form.value)))
+            // it emit multiples requests to the server
+            // .pipe(concatMap(() => this.saveCourse(this.form.value)))
+            .subscribe()
+    }
 
     save() {
-        console.log('save')
+        //console.log('save')
     }
 
     close() {
