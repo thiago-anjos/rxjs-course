3c2d75aec09ffeb3a76bf19aabc1502b9e570e6b
thiago <thiago.w3c@gmail.com>
2023-11-02 18:53:03 -0300

    feat: concat the initial lessons and searched

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,8 +7,14 @@ import {
 } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Course } from '../model/course'
-import { Observable, fromEvent } from 'rxjs'
-import { map, tap, debounceTime, distinctUntilChanged } from 'rxjs/operators'
+import { Observable, concat, fromEvent } from 'rxjs'
+import {
+    map,
+    tap,
+    debounceTime,
+    distinctUntilChanged,
+    switchMap,
+} from 'rxjs/operators'
 import { Lesson } from '../model/lesson'
 import { createHttpObservable } from '../utils/httpObservable'
 
@@ -20,30 +26,38 @@ import { createHttpObservable } from '../utils/httpObservable'
 export class CourseComponent implements OnInit, AfterViewInit {
     course$: Observable<Course[]>
     lessons$: Observable<Lesson[]>
+    public courseId: string
 
     @ViewChild('searchInput', { static: true }) input: ElementRef
 
     constructor(private route: ActivatedRoute) {}
 
     ngOnInit() {
-        const courseId = this.route.snapshot.params['id']
-        this.course$ = createHttpObservable(`/api/courses/${courseId}`)
-
-        this.lessons$ = createHttpObservable(
-            `/api/lessons?courseId=${courseId}`
-        ).pipe(
-            tap(console.log),
-            map((res) => res['payload'])
-        )
+        this.courseId = this.route.snapshot.params['id']
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
     }
 
     ngAfterViewInit() {
-        fromEvent<any>(this.input.nativeElement, 'keyup')
-            .pipe(
-                map((event) => event.target.value),
-                debounceTime(400), // wait 400 miliseconds until log it to the console
-                distinctUntilChanged() // prevent duplicates values in output
-            )
-            .subscribe(console.log)
+        const searchedLessons$ = fromEvent<any>(
+            this.input.nativeElement,
+            'keyup'
+        ).pipe(
+            map((event) => event.target.value),
+            debounceTime(400), // wait 400 miliseconds until log it to the console
+            distinctUntilChanged(), // prevent duplicates values in output
+            switchMap((search) => this.loadLessons(search)) // only send the last emmited values, cancelling the others
+        )
+
+        const initialLessons$ = (this.lessons$ = this.loadLessons())
+
+        this.lessons$ = concat(initialLessons$, searchedLessons$)
+    }
+
+    loadLessons(search: string = ''): Observable<Lesson[]> {
+        const url = `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`
+        return createHttpObservable(url).pipe(
+            tap(console.log),
+            map((res) => res['payload'])
+        )
     }
 }
