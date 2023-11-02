ddf35aafca9979fbd44db6f14aa9648070171f77
thiago <thiago.w3c@gmail.com>
2023-11-02 18:35:51 -0300

    feat: implement observable at input search with control of debounce and duplicate calls

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,57 +1,49 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
 import {
-    debounceTime,
-    distinctUntilChanged,
-    startWith,
-    tap,
-    delay,
-    map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+    AfterViewInit,
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild,
+} from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Course } from '../model/course'
+import { Observable, fromEvent } from 'rxjs'
+import { map, tap, debounceTime, distinctUntilChanged } from 'rxjs/operators'
+import { Lesson } from '../model/lesson'
+import { createHttpObservable } from '../utils/httpObservable'
 
 @Component({
     selector: 'course',
     templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css']
+    styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit, AfterViewInit {
+    course$: Observable<Course[]>
+    lessons$: Observable<Lesson[]>
 
+    @ViewChild('searchInput', { static: true }) input: ElementRef
 
-    course$: Observable<Course>;
-    lessons$: Observable<Lesson[]>;
-
-
-    @ViewChild('searchInput', { static: true }) input: ElementRef;
-
-    constructor(private route: ActivatedRoute) {
-
-
-    }
+    constructor(private route: ActivatedRoute) {}
 
     ngOnInit() {
+        const courseId = this.route.snapshot.params['id']
+        this.course$ = createHttpObservable(`/api/courses/${courseId}`)
 
-        const courseId = this.route.snapshot.params['id'];
-
-
-
+        this.lessons$ = createHttpObservable(
+            `/api/lessons?courseId=${courseId}`
+        ).pipe(
+            tap(console.log),
+            map((res) => res['payload'])
+        )
     }
 
     ngAfterViewInit() {
-
-
-
-
+        fromEvent<any>(this.input.nativeElement, 'keyup')
+            .pipe(
+                map((event) => event.target.value),
+                debounceTime(400), // wait 400 miliseconds until log it to the console
+                distinctUntilChanged() // prevent duplicates values in output
+            )
+            .subscribe(console.log)
     }
-
-
-
-
 }
